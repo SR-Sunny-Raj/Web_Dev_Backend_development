ff4afd9857c15d0ad9b6fe388fdc71f278eb4dda
Sunny Raj <SRAJ1333.SR@GMAIL.COM>
2021-06-29 13:09:27 +0530

    serving html files

---
+++ Node.js/serving_html_files.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const fs = require('fs');
+const fileContent = fs.readFileSync('Node.js\\index1.html');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-type': 'text/html' });
+  res.end(fileContent);
+});
+server.listen(80, '127.0.0.1', () => {
+  console.log('Listening at port 80');
+});
