cb7a103056b91544429013f327a48fc19b9c5043
Sunny Raj <SRAJ1333.SR@GMAIL.COM>
2021-06-29 19:55:52 +0530

    creating custom modules in node.js

---
+++ Node.js/creating custom module in node using node js/index.js
@@ -0,0 +1,4 @@
+const mod = require('.\\mod.js');
+console.log(mod.avg([5, 3]));
+console.log(mod.name);
+console.log(mod.role);
---
+++ Node.js/creating custom module in node using node js/mod.js
@@ -0,0 +1,12 @@
+function average(arr) {
+  let sum = 0;
+  arr.forEach((element) => {
+    sum += element;
+  });
+  return sum / arr.length;
+}
+module.exports = {
+  avg: average,
+  name: 'Sunny',
+  role: 'Student',
+};
