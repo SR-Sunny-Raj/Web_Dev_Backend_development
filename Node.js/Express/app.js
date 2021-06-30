5ff114b0333ca8e60aa5ea0430c6bece84138aa2
Sunny Raj <SRAJ1333.SR@GMAIL.COM>
2021-06-30 16:17:04 +0530

    Express app

--- Node.js/Express/app.js
+++ Node.js/Express/app.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 80;
+// For Serving Static files
+app.use('/static', express.static('static'));
+
+// Set the template engine as pug
+app.set('view engine', 'pug');
+
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// Our pug demo endpoint
+app.get('/demo', (req, res) => {
+  res
+    .status(200)
+    .render('demo', { title: 'Hey Sunny', message: 'Hello there' });
+});
+
 app.get('/', (req, res) => {
   res.send('This is the homepage of our first express app');
 });
