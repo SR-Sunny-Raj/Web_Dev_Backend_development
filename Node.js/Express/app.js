5434e4fdfda377c1d8d75a03e77199ee31c0e8b9
Sunny Raj <SRAJ1333.SR@GMAIL.COM>
2021-07-02 22:57:25 +0530

    Express

--- Node.js/Express/app.js
+++ Node.js/Express/app.js
@@ -3,33 +3,39 @@ const app = express();
 const path = require('path');
 const port = 80;
 // For Serving Static files
+// EXPRESS SPECIFIC STUFFS
 app.use('/static', express.static('static'));
 
 // Set the template engine as pug
+// PUG SPECIFIC STUFFS
 app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views')); // Set the views directory
 
-// Set the views directory
-app.set('views', path.join(__dirname, 'views'));
-
+const con = 'This is the best content';
+const params = { title: 'Pubg is the best game', content: con };
 // Our pug demo endpoint
-app.get('/demo', (req, res) => {
-  res
-    .status(200)
-    .render('demo', { title: 'Hey Sunny', message: 'Hello there' });
+// ENDPOINTS
+// app.get('/demo', (req, res) => {
+//   res
+//     .status(200)
+//     .render('demo', { title: 'Hey Sunny', message: 'Hello there' });
+// });
+app.get('/', (req, res) => {
+  res.status(200).render('index.pug', params);
 });
 
-app.get('/', (req, res) => {
-  res.send('This is the homepage of our first express app');
-});
-app.get('/about', (req, res) => {
-  res.send('This is the about page of our first express app');
-});
-app.post('/about', (req, res) => {
-  res.send('This is a post request about page of our first express app');
-});
-app.get('/this', (req, res) => {
-  res.status(404).send('The page is not found');
-});
+// app.get('/', (req, res) => {
+//   res.send('This is the homepage of our first express app');
+// });
+// app.get('/about', (req, res) => {
+//   res.send('This is the about page of our first express app');
+// });
+// app.post('/about', (req, res) => {
+//   res.send('This is a post request about page of our first express app');
+// });
+// app.get('/this', (req, res) => {
+//   res.status(404).send('The page is not found');
+// });
 
 app.listen(port, () => {
   console.log(`The application started at port ${port}`);
