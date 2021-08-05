74e0524d64ad61f71d5cfb0a9674afc270aded32
Sunny Raj <SRAJ1333.SR@GMAIL.COM>
2021-08-05 10:07:46 +0530

    Removed some unnecessary files

--- Node_js_stuffs/Bootcamp/todolist-v1/app.js
+++ Node_js_stuffs/Bootcamp/todolist-v1/app.js
@@ -4,26 +4,14 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-var today = new Date();
-var currentDay = today.getDay();
-var day = '';
-
 app.get('/', (req, res) => {
-  if (currentDay === 0) {
-    tay = 'Sunday';
-  } else if (currentDay === 1) {
-    day = 'Monday';
-  } else if (currentDay === 2) {
-    day = 'Tuesday';
-  } else if (currentDay === 3) {
-    day = 'Wednesday';
-  } else if (currentDay === 4) {
-    day = 'Thursday';
-  } else if (currentDay === 5) {
-    day = 'Friday';
-  } else if (currentDay === 6) {
-    day = 'Saturday';
-  }
+  var today = new Date();
+  var options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+  };
+  var day = today.toLocaleDateString('en-US', options);
   res.render('list', { kindOfDay: day });
 });
 
