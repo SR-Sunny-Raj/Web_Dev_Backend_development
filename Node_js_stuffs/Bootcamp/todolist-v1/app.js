c494887d2d86f6143c725f6a3f0b926aa0fd268e
Sunny Raj <SRAJ1333.SR@GMAIL.COM>
2021-08-05 21:55:36 +0530

    To-Do List Web App

--- Node_js_stuffs/Bootcamp/todolist-v1/app.js
+++ Node_js_stuffs/Bootcamp/todolist-v1/app.js
@@ -1,18 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const getDate = require('./date');
 const app = express();
+
+let items = ['Buy Food', 'Cook Food', 'Eat Food'];
+let workItems = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  var today = new Date();
-  var options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-  };
-  var day = today.toLocaleDateString('en-US', options);
-  res.render('list', { kindOfDay: day });
+  let day = getDate();
+  res.render('list', { listTitle: day, newListItems: items });
+});
+
+app.post('/', (req, res) => {
+  let item = req.body.newItem;
+  if (req.body.list === 'Work') {
+    workItems.push(item);
+    res.redirect('/work');
+  } else {
+    items.push(item);
+    res.redirect('/');
+  }
+});
+
+app.get('/work', (req, res) => {
+  res.render('list', { listTitle: 'Work List', newListItems: workItems });
 });
 
 app.listen(3000, () => {
